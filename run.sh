ab3afff6c13da5007b7e826ae80e706485c9271a
Shane Mulligan <mullikine@gmail.com>
2020-08-28 14:54:35 +1200

    Fri 28 Aug 2020 14:54:35 NZST

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+host_shared_dir="$(realpath "$(pwd)")"
 container_shared_dir="/$(pwd | slugify)"
 
-docker run --rm --privileged "--network=host" -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd):/$(pwd | slugify)" -ti "--entrypoint=" openai-gpt-2-e12a391b:1.0 /run_jupyter.sh --allow-root
+docker \
+    run \
+    --rm \
+    --privileged \
+    "--network=host" \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$host_shared_dir:/$container_shared_dir" \
+    -ti \
+    "--entrypoint=" \
+    openai-gpt-2-e12a391b:1.0 \
+    /run_jupyter.sh --allow-root
